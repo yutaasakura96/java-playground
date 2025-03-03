974a2f731b0141b177b570d0a15f9ad6d2a80581
yuta asakura <yuta.asakura.se@gmail.com>
2025-03-03 23:08:33 +0900

    Add example demonstrating variable assignment in Java

---
+++ fundamentals/variables/com/variables/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+  public static void main(String args[]) {
+    int myAge = 30;
+    int herAge = myAge;
+
+    System.out.println("She is " + herAge + " years old today");
+  }
+}
+
