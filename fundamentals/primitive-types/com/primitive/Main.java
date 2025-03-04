d00a35d403aa3a6c049eeba707fc035560e487b1
yuta asakura <yuta.asakura.se@gmail.com>
2025-03-04 11:09:56 +0900

    Add Main class example demonstrating primitive data types in Java

---
+++ fundamentals/primitive-types/com/primitive/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+  public static void main(String args[]) {
+    byte age = 30;
+    long viewsCount = 112_500_252L;
+    float price = 10.52F;
+    char letter = 'R';
+    boolean isElgible = false;
+
+    System.out.println(age);
+    System.out.println(viewsCount);
+    System.out.println(price);
+    System.out.println(letter);
+    System.out.println(isElgible);
+  }
+}
