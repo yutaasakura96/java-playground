a70a55321524256ccfd124cd56561ac3541089b6
yuta asakura <yuta.asakura.se@gmail.com>
2025-03-04 14:38:50 +0900

    Add example demonstrating array creation and manipulation in Java

---
+++ fundamentals/com/javabasics/Arrays.java
@@ -0,0 +1,27 @@
+public class Arrays {
+  public static void main(String args[]) {
+    int[] oldArray = oldArray();
+    int[] newArray = newArray();
+    System.out.println(java.util.Arrays.toString(oldArray));
+    System.out.println(java.util.Arrays.toString(newArray));
+    System.out.println("New array length:" + newArray.length);
+    java.util.Arrays.sort(newArray);
+    System.out.println("Sorted New array:" + java.util.Arrays.toString(newArray));
+
+  }
+
+  public static int[] oldArray() {
+    int[] numbers = new int[5];
+    numbers[0] = 1;
+    numbers[1] = 2;
+    numbers[2] = 3;
+    numbers[3] = 4;
+    numbers[4] = 5;
+    return numbers;
+  }
+
+  public static int[] newArray() {
+    int[] numbers = { 8, 4, 7, 2, 6 };
+    return numbers;
+  }
+}
