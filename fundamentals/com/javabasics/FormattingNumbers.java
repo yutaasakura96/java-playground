5c0d3b0412d4b24149a72b450019250bafa3b6f0
yuta asakura <yuta.asakura.se@gmail.com>
2025-03-04 23:12:43 +0900

    Add example demonstrating number formatting using NumberFormat in Java

---
+++ fundamentals/com/javabasics/FormattingNumbers.java
@@ -0,0 +1,10 @@
+
+import java.text.NumberFormat;
+
+public class FormattingNumbers {
+  public static void main(String[] args) {
+      System.out.println("Formatting number 12412412411412 using NumberFormat.getCurrencyInstance() " + NumberFormat.getCurrencyInstance().format(12412412411412L));
+
+      System.out.println("Formatting number 0.25 using NumberFormat.getPercentInstance() " + NumberFormat.getPercentInstance().format(0.25));
+  }
+}
