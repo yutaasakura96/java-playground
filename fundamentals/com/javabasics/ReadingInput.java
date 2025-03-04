90ef0afc86ff7a88e93f97a08f56f871780e6135
yuta asakura <yuta.asakura.se@gmail.com>
2025-03-05 00:03:39 +0900

    Add example demonstrating reading input from the console in Java

---
+++ fundamentals/com/javabasics/ReadingInput.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class ReadingInput {
+  public static void main(String[] args) {
+      Scanner scanner = new Scanner(System.in);
+      System.out.print("What is your name? ");
+      String name = scanner.nextLine().trim();
+      System.out.println("Hello, " + name);
+      System.out.print("How old are you? ");
+      int age = scanner.nextInt();
+      System.out.println("Hello, " + name + " You are " + age + " years old");
+  }
+}
