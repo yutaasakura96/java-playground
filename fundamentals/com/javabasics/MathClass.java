480cb36686d03504a2075dc66c0e24c1944a1ac4
yuta asakura <yuta.asakura.se@gmail.com>
2025-03-04 22:53:52 +0900

    Add example demonstrating the use of the Math class in Java

---
+++ fundamentals/com/javabasics/MathClass.java
@@ -0,0 +1,14 @@
+public class MathClass {
+  public static void main(String[] args) {
+    int x = 1;
+    int y = 2;
+    float z = (float) 24.555;
+
+    System.out.println("Math.max result: " + Math.max(x, y));
+    System.out.println("Math.min result: " + Math.min(x, y));
+    System.out.println("Math.floor result: " + Math.floor((double)z));
+    System.out.println("Math.ceil result: " + Math.ceil((double)z));
+    System.out.println("Math.random result: " + Math.random());
+    System.out.println("Generating a random number between 0 and 100: " + (int) (Math.random() * 100));
+  }
+}
