6092ce3f8c4b0607782b63ee3ab00864ef24be6e
yuta asakura <yuta.asakura.se@gmail.com>
2025-03-05 17:08:19 +0900

    Add example demonstrating the use of switch statements in Java

---
+++ fundamentals/com/javabasics/SwitchStatement.java
@@ -0,0 +1,17 @@
+public class SwitchStatement {
+  public static void main(String[] args) {
+    int day = (int)(Math.random() * 7) + 1;
+    String dayName = switch (day) {
+      case 1 -> "Sunday";
+      case 2 -> "Monday";
+      case 3 -> "Tuesday";
+      case 4 -> "Wednesday";
+      case 5 -> "Thursday";
+      case 6 -> "Friday";
+      case 7 -> "Saturday";
+      default -> "Invalid day";
+    };
+
+    System.out.println("The day is: " + dayName);
+  }
+}
