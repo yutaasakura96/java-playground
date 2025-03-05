ddc362363972c459adbee92dde11f96ac7e62e0e
yuta asakura <yuta.asakura.se@gmail.com>
2025-03-05 19:00:27 +0900

    Add input validation for principal, annual interest rate, and period in MortgageCalculator

--- fundamentals/fundamentals/mortgagecalculator/MortgageCalculator.java
+++ fundamentals/fundamentals/mortgagecalculator/MortgageCalculator.java
@@ -11,15 +11,27 @@ public class MortgageCalculator {
 
     Scanner scanner = new Scanner(System.in);
 
-    System.out.print("Principal: ");
+    System.out.print("Principal ($1K - $1M): ");
     int principal = scanner.nextInt();
+    while (principal < 1000 || principal > 1_000_000) {
+      System.out.println("Please enter a number between 1,000 and 1,000,000.");
+      principal = scanner.nextInt();
+    }
 
     System.out.print("Annual Interest Rate: ");
     float annualInterest = scanner.nextFloat();
+    while (annualInterest <= 0 || annualInterest > 30) {
+      System.out.println("Please enter a value greater than 0 and less than or equal to 30.");
+      annualInterest = scanner.nextFloat();
+    }
     float monthlyInterest = annualInterest / PERCENT / MONTHS_IN_YEAR;
 
     System.out.print("Period (Years): ");
     byte years = scanner.nextByte();
+    while (years < 1 || years > 30) {
+      System.out.print("Period (Years): ");
+      years = scanner.nextByte();
+    }
     int numberOfPayments = years * MONTHS_IN_YEAR;
 
     double mortgage =
