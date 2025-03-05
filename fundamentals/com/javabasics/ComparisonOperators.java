c8c71f7f6d525f574595b0ae5aabe9ea87b68ea0
yuta asakura <yuta.asakura.se@gmail.com>
2025-03-05 14:28:46 +0900

    Add example demonstrating comparison operators in Java

---
+++ fundamentals/com/javabasics/ComparisonOperators.java
@@ -0,0 +1,14 @@
+public class ComparisonOperators {
+  public static void main(String[] args) {
+      int x = 1;
+      int y = 1;
+      int z = 3;
+
+      System.out.println("x == y: " + (x == y));
+      System.out.println("x != y: " + (x != y)); 
+      System.out.println("x > y: " + (x > y));
+      System.out.println("x < z: " + (x < z));
+      System.out.println("x >= y: " + (x >= y));
+      System.out.println("x <= z: " + (x <= z));
+  }
+}
