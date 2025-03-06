8b7cabb10a9632a431b9b7dd7abc3ae539659356
yuta asakura <yuta.asakura.se@gmail.com>
2025-03-06 21:31:27 +0900

    Refactor main method, Add mortgage and payment schedule printing methods to Main class

--- fundamentals/mortgage-calculator/src/mortgagecalculator/Main.java
+++ fundamentals/mortgage-calculator/src/mortgagecalculator/Main.java
@@ -17,13 +17,20 @@ public class Main {
     annualInterest = (float) readNumber("Annual Interest Rate: ", 1, 30);
     years = (byte) readNumber("Period (Years): ", 1, 30);
 
+    printMortgage(principal, annualInterest, years);
+    printPaymentSchedule(principal, annualInterest, years);
+  }
+
+  private static void printMortgage(int principal, float annualInterest, byte years) {
     double mortgage = calculateMortgage(principal, annualInterest, years);
     String mortgageFormatted = NumberFormat.getCurrencyInstance().format(mortgage);
     System.out.println();
     System.out.println("MORTGAGE");
     System.out.println("--------");
     System.out.println("Monthly Payments: " + mortgageFormatted);
+  }
 
+  private static void printPaymentSchedule(int principal, float annualInterest, byte years) {
     System.out.println();
     System.out.println("PAYMENT SCHEDULE");
     System.out.println("----------------");
