b2326a789a0ad9fe9170469166211a8e7c4b89fd
yuta asakura <yuta.asakura.se@gmail.com>
2025-03-06 20:48:09 +0900

    Refactor input handling in Main class to use readNumber method for validation

--- fundamentals/mortgage-calculator/src/mortgagecalculator/Main.java
+++ fundamentals/mortgage-calculator/src/mortgagecalculator/Main.java
@@ -6,43 +6,32 @@ import java.util.Scanner;
 public class Main {
 
   public static void main(String[] args) {
-
     int principal = 0;
     float annualInterest = 0;
     byte years = 0;
 
-    Scanner scanner = new Scanner(System.in);
-
-    while (true) {
-      System.out.print("Principal: ");
-      principal = scanner.nextInt();
-      if (principal >= 1000 && principal <= 1_000_000)
-        break;
-      System.out.println("Enter a value between 1000 and 1000000");
-    }
-
-    while (true) {
-      System.out.print("Annual Interest Rate: ");
-      annualInterest = scanner.nextFloat();
-      if (annualInterest >= 1 && annualInterest <= 30) {
-        break;
-      }
-      System.out.println("Enter a value between 1 and 30");
-    }
-
-    while (true) {
-      System.out.print("Period (Years): ");
-      years = scanner.nextByte();
-      if (years >= 1 && years <= 30) {
-        break;
-      }
-      System.out.println("Enter a value between 1 and 30.");
-    }
+    principal = (int) readNumber("Principal: ", 1000, 1_000_000);
+    annualInterest = (float) readNumber("Annual Interest Rate: ", 1, 30);
+    years = (byte) readNumber("Period (Years): ", 1, 30);
 
     double mortgage = calculateMortgage(principal, annualInterest, years);
 
     String mortgageFormatted = NumberFormat.getCurrencyInstance().format(mortgage);
     System.out.println("Mortgage: " + mortgageFormatted);
+  }
+
+  public static double readNumber(String prompt, int min, int max) {
+    Scanner scanner = new Scanner(System.in);
+    double value;
+
+    while (true) {
+      System.out.print(prompt);
+      value = scanner.nextInt();
+      if (value >= min && value <= max)
+        break;
+      System.out.println("Enter a value between" + min + " and " + max);
+    }
+    return value;
   }
 
   public static double calculateMortgage(int principal, float annualInterest, byte years) {
