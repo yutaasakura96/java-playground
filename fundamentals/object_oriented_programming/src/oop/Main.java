af4b6c75f5e673fda5497b6852ea9b70d5f4ac3b
yuta asakura <yuta.asakura.se@gmail.com>
2025-03-07 16:00:47 +0900

    Add TextBox usage in main method for demonstration

--- fundamentals/object_oriented_programming/src/oop/Main.java
+++ fundamentals/object_oriented_programming/src/oop/Main.java
@@ -3,5 +3,13 @@ package oop;
 public class Main {
   public static void main(String[] args) {
       System.out.println("This is the main class");
+
+      var textBox1 = new TextBox();
+      textBox1.setText("Box: 1");
+      System.out.println(textBox1.text.toUpperCase());
+
+      var textBox2 = new TextBox();
+      textBox2.setText("Box: 2");
+      System.out.println(textBox2.text);
   }
 }
