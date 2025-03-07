b7eef08ad32f902e1a6b3e67b51668462805f925
yuta asakura <yuta.asakura.se@gmail.com>
2025-03-07 17:00:33 +0900

    Add Employee class for wage calculation and update Main class to demonstrate encapsulation

--- fundamentals/object_oriented_programming/src/oop/Main.java
+++ fundamentals/object_oriented_programming/src/oop/Main.java
@@ -2,14 +2,13 @@ package oop;
 
 public class Main {
   public static void main(String[] args) {
-      System.out.println("This is the main class");
+    var employee = new Employee();
+    employee.baseSalary = 50_000;
+    employee.hourlyRate = 20;
 
-      var textBox1 = new TextBox();
-      textBox1.setText("Box: 1");
-      System.out.println(textBox1.text.toUpperCase());
-
-      var textBox2 = new TextBox();
-      textBox2.setText("Box: 2");
-      System.out.println(textBox2.text);
+    int wage = employee.calculateWage(10);
+    System.out.println(wage);
   }
+
+
 }
