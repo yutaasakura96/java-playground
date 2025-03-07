b70e1167915d0707e5f64159442027e454eda2bc
yuta asakura <yuta.asakura.se@gmail.com>
2025-03-07 16:23:50 +0900

    Procedural programming demonstration Add WageCalculator class with wage calculation functionality

---
+++ fundamentals/object_oriented_programming/src/oop/WageCalculator.java
@@ -0,0 +1,18 @@
+package oop;
+
+public class WageCalculator {
+  public static void main(String[] args) {
+    int baseSalary = 50_000;
+    int extraHours = 10;
+    int hourlyRate = 20;
+
+    int wage = calculateWage(baseSalary, extraHours, hourlyRate);
+
+    System.out.println(wage);
+
+  }
+
+  public static int calculateWage(int baseSalary, int extraHours, int hourlyRate) {
+    return baseSalary + (extraHours * hourlyRate);
+  }
+}
