aa86d3e35e40a3444deb59755ed751aaf05bbe3e
yuta asakura <yuta.asakura.se@gmail.com>
2025-03-07 17:23:20 +0900

    Encapsulate Employee class fields and add setter/getter methods for base salary and hourly rate

--- fundamentals/object_oriented_programming/src/oop/Employee.java
+++ fundamentals/object_oriented_programming/src/oop/Employee.java
@@ -1,10 +1,30 @@
 package oop;
 
 public class Employee {
-  public int baseSalary;
-  public int hourlyRate;
+  private int baseSalary;
+  private int hourlyRate;
 
   public int calculateWage(int extraHours) {
     return baseSalary + (extraHours * hourlyRate);
+  }
+
+  public void setBaseSalary(int baseSalary) {
+    if (baseSalary <= 0)
+      throw new IllegalArgumentException("Salary cannot be 0 or less.");
+    this.baseSalary = baseSalary;
+  }
+
+  public int getBaseSalary() {
+    return baseSalary;
+  }
+
+  public void setHourlyRate(int hourlyRate) {
+    if (hourlyRate <=0)
+      throw new IllegalArgumentException("Hourly rate cannot be 0 or less.");
+    this.hourlyRate = hourlyRate;
+  }
+
+  public int getHourlyRate() {
+    return hourlyRate;
   }
 }
--- fundamentals/object_oriented_programming/src/oop/Main.java
+++ fundamentals/object_oriented_programming/src/oop/Main.java
@@ -3,8 +3,8 @@ package oop;
 public class Main {
   public static void main(String[] args) {
     var employee = new Employee();
-    employee.baseSalary = 50_000;
-    employee.hourlyRate = 20;
+    employee.setBaseSalary(50_000);
+    employee.setHourlyRate(20);
 
     int wage = employee.calculateWage(10);
     System.out.println(wage);
