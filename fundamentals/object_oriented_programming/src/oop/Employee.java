2ff3624c03049bf8401f9fbee662f7566e9d0974
yuta asakura <yuta.asakura.se@gmail.com>
2025-03-08 20:33:38 +0900

    Add Browser class for demonstrating reducing coupling through abstraction

--- fundamentals/object_oriented_programming/src/oop/Employee.java
+++ fundamentals/object_oriented_programming/src/oop/Employee.java
@@ -14,7 +14,7 @@ public class Employee {
     this.baseSalary = baseSalary;
   }
 
-  public int getBaseSalary() {
+  private  int getBaseSalary() {
     return baseSalary;
   }
 
@@ -24,7 +24,7 @@ public class Employee {
     this.hourlyRate = hourlyRate;
   }
 
-  public int getHourlyRate() {
+  private int getHourlyRate() {
     return hourlyRate;
   }
 }
--- fundamentals/object_oriented_programming/src/oop/Main.java
+++ fundamentals/object_oriented_programming/src/oop/Main.java
@@ -9,6 +9,4 @@ public class Main {
     int wage = employee.calculateWage(10);
     System.out.println(wage);
   }
-
-
 }
