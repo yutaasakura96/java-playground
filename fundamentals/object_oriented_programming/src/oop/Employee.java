6f0a56492e9f5a5cac2c6c9a7b93d0168811fe92
yuta asakura <yuta.asakura.se@gmail.com>
2025-03-08 20:59:40 +0900

    Add new constructor for Employee class with base salary paramater only and update Main class to demonstrate constructor overloading.

--- fundamentals/object_oriented_programming/src/oop/Employee.java
+++ fundamentals/object_oriented_programming/src/oop/Employee.java
@@ -9,6 +9,10 @@ public class Employee {
     setHourlyRate(hourlyRate);
   }
 
+  public Employee(int baseSalary) {
+    this(baseSalary, 0);
+  }
+
   public int calculateWage(int extraHours) {
     return baseSalary + (extraHours * hourlyRate);
   }
@@ -28,7 +32,7 @@ public class Employee {
   }
 
   private void setHourlyRate(int hourlyRate) {
-    if (hourlyRate <=0)
+    if (hourlyRate <0)
       throw new IllegalArgumentException("Hourly rate cannot be 0 or less.");
     this.hourlyRate = hourlyRate;
   }
--- fundamentals/object_oriented_programming/src/oop/Main.java
+++ fundamentals/object_oriented_programming/src/oop/Main.java
@@ -3,12 +3,12 @@ package oop;
 public class Main {
   public static void main(String[] args) {
     var employee = new Employee(50_000,20);
-    var employee2 = new Employee(35_000, 15);
+    var employee2 = new Employee(35_000);
 
     int wage = employee.calculateWage(10);
     int wage2 = employee2.calculateWage();
 
-    System.out.println(wage);
-    System.out.println(wage2);
+    System.out.println("Employee 1: " + wage);
+    System.out.println("Employee 2: " + wage2);
   }
 }
