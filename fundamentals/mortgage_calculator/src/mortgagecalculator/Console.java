872cd3f92970ad4b94167208b54b8396cd484fcc
yuta asakura <yuta.asakura.se@gmail.com>
2025-03-11 17:15:02 +0900

    Refactor Console class to use a static Scanner instance and update readNumber method for improved input handling

--- fundamentals/mortgage_calculator/src/mortgagecalculator/Console.java
+++ fundamentals/mortgage_calculator/src/mortgagecalculator/Console.java
@@ -3,13 +3,19 @@ package mortgagecalculator;
 import java.util.Scanner;
 
 public class Console {
+
+  private static Scanner scanner = new Scanner(System.in);
+
+  public static double readNumber() {
+    return scanner.nextDouble();
+  }
+
   public static double readNumber(String prompt, int min, int max) {
-    Scanner scanner = new Scanner(System.in);
     double value;
 
     while (true) {
       System.out.print(prompt);
-      value = scanner.nextInt();
+      value = scanner.nextDouble();
       if (value >= min && value <= max)
         break;
       System.out.println("Enter a value between" + min + " and " + max);
