03c7eaaf08c61c6abb4d068ceb6c80e173eb55cf
yuta asakura <yuta.asakura.se@gmail.com>
2025-03-12 22:20:19 +0900

    Refactor MortgageReport to use a single NumberFormat instance for improved performance and readability

--- fundamentals/mortgage_calculator/src/mortgagecalculator/MortgageReport.java
+++ fundamentals/mortgage_calculator/src/mortgagecalculator/MortgageReport.java
@@ -4,15 +4,17 @@ import java.text.NumberFormat;
 
 public class MortgageReport {
 
+  private final NumberFormat currency;
   private MortgageCalculator calculator;
 
   public MortgageReport(MortgageCalculator calculator) {
     this.calculator = calculator;
+    currency = NumberFormat.getCurrencyInstance();
   }
 
   public void printMortgage() {
     double mortgage = calculator.calculateMortgage();
-    String mortgageFormatted = NumberFormat.getCurrencyInstance().format(mortgage);
+    String mortgageFormatted = currency.format(mortgage);
     System.out.println();
     System.out.println("MORTGAGE");
     System.out.println("--------");
@@ -24,7 +26,7 @@ public class MortgageReport {
     System.out.println("PAYMENT SCHEDULE");
     System.out.println("----------------");
     for (double balance : calculator.getRemainingBalances()) {
-      System.out.println(NumberFormat.getCurrencyInstance().format(balance));
+      System.out.println(currency.format(balance));
     }
   }
 }
