935f89610ad11b79f176d991ef754c3ac9bcb02d
yuta asakura <yuta.asakura.se@gmail.com>
2025-03-30 15:29:16 +0900

    Add file reading example with exception handling in ExceptionsDemo

--- fundamentals/exceptions/src/ExceptionsDemo.java
+++ fundamentals/exceptions/src/ExceptionsDemo.java
@@ -1,8 +1,16 @@
 package fundamentals.exceptions.src;
 
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+
 public class ExceptionsDemo {
   public static void show() {
-    sayHello(null);
+    try {
+      var reader = new FileReader("file.txt");
+    } catch (FileNotFoundException e) {
+      // TODO Auto-generated catch block
+      e.printStackTrace();
+    }
   }
 
   public static void sayHello(String name) {
