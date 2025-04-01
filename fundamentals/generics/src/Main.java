3f6472cb2ead473ad2bfe5e8c716ac08b2985303
yuta asakura <yuta.asakura.se@gmail.com>
2025-04-01 10:53:34 +0900

    Remove unused IDE configuration files and clean up project structure

--- fundamentals/generics/src/Main.java
+++ fundamentals/generics/src/Main.java
@@ -2,8 +2,8 @@ package fundamentals.generics.src;
 
 public class Main {
   public static void main(String[] args) {
-    var list = new GenericList<Integer>();
-    list.add(1);
-    int number = list.get(0);
-  }
+    GenericList<Integer> numbers = new GenericList<>();
+    numbers.add(1);
+    int number = numbers.get(0);
+   }
 }
