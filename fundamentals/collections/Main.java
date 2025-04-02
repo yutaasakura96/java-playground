973f927ca5aec783388f6dae52819d8994fafe1b
yuta asakura <yuta.asakura.se@gmail.com>
2025-04-02 13:37:41 +0900

    Add Customer class implementing Comparable and update Main to demonstrate sorting

--- fundamentals/collections/Main.java
+++ fundamentals/collections/Main.java
@@ -1,5 +1,14 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 public class Main {
   public static void main(String[] args) {
-    ListDemo.show();
+    List<Customer> customers = new ArrayList<>();
+    customers.add(new Customer("b"));
+    customers.add(new Customer("a"));
+    customers.add(new Customer("c"));
+    Collections.sort(customers);
+    System.out.println(customers);
   }
 }
