9b574706c42345c1824bade42236a37315530410
yuta asakura <yuta.asakura.se@gmail.com>
2025-04-03 16:10:26 +0900

    Add Printer interface and implement ConsolePrinter for message output

---
+++ fundamentals/lambdas/LambdasDemo.java
@@ -0,0 +1,9 @@
+public class LambdasDemo {
+  public static void show() {
+    greet(new ConsolePrinter());
+  }
+
+  public static void greet(Printer printer) {
+    printer.print("Hello World");
+  }
+}
