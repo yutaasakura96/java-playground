b59e2f80d9c3ffc4236f047c8a990a4e1ca2556c
yuta asakura <yuta.asakura.se@gmail.com>
2025-04-03 18:59:52 +0900

    Refactor LambdasDemo to use a lambda expression for the Printer implementation

--- fundamentals/lambdas/LambdasDemo.java
+++ fundamentals/lambdas/LambdasDemo.java
@@ -1,11 +1,15 @@
 public class LambdasDemo {
   public static void show() {
+    greet(message -> System.out.println(message));
+
     greet(new Printer() {
       @Override
       public void print(String message) {
         System.out.println(message);
       }
     });
+
+    Printer printer = message -> System.out.println(message);
   }
 
   public static void greet(Printer printer) {
