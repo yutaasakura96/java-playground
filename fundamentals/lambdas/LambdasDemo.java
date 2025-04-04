602f80d4670a6ea08b9d46b9cd1526ae31eaed68
yuta asakura <yuta.asakura.se@gmail.com>
2025-04-04 18:50:56 +0900

    Simplify LambdasDemo by removing anonymous Printer implementation and add Main class to execute the demo

--- fundamentals/lambdas/LambdasDemo.java
+++ fundamentals/lambdas/LambdasDemo.java
@@ -1,15 +1,6 @@
 public class LambdasDemo {
   public static void show() {
     greet(message -> System.out.println(message));
-
-    greet(new Printer() {
-      @Override
-      public void print(String message) {
-        System.out.println(message);
-      }
-    });
-
-    Printer printer = message -> System.out.println(message);
   }
 
   public static void greet(Printer printer) {
