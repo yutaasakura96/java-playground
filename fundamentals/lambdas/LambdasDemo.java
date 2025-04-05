4b1efaecc00176f0160def816e300e0faab4aca2
yuta asakura <yuta.asakura.se@gmail.com>
2025-04-05 16:02:54 +0900

    Refactor LambdasDemo to use a list of strings and demonstrate method chaining with Consumers

--- fundamentals/lambdas/LambdasDemo.java
+++ fundamentals/lambdas/LambdasDemo.java
@@ -1,18 +1,15 @@
 
 import java.util.List;
+import java.util.function.Consumer;
 
 public class LambdasDemo {
 
   public static void show() {
-    List<Integer> list = List.of(1,2,3);
+    List<String> list = List.of("a", "b", "c");
+    Consumer<String> print = item -> System.out.println(item);
+    Consumer<String> printUpperCase = item -> System.out.println(item.toUpperCase());
 
-    for (var item : list ) {
-      System.out.println(item);
-    }
-
-    list.forEach(item -> {
-      System.out.println(item);
-    });
+    list.forEach(print.andThen(printUpperCase).andThen(print));
   }
 
   public static void greet(Printer printer) {
