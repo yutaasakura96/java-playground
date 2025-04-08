8e4bb5dc452f2a76caa7252922ad3d0ed785bc07
yuta asakura <yuta.asakura.se@gmail.com>
2025-04-08 16:27:45 +0900

    Refactor StreamsDemo to remove movie counting logic and demonstrate random number generation

--- fundamentals/streams/StreamsDemo.java
+++ fundamentals/streams/StreamsDemo.java
@@ -1,22 +1,10 @@
 package fundamentals.streams;
 
-import java.util.List;
+import java.util.stream.Stream;
 
 public class StreamsDemo {
   public static void show() {
-    List<Movie> movies = List.of(new Movie("a", 10), new Movie("b", 15), new Movie("c", 20));
-    int count = 0;
-
-    for (var movie : movies) {
-      if (movie.getLikes() > 10) {
-        count++;
-      }
-    }
-
-    System.out.println(count);
-
-    var count2 = movies.stream().filter(movie -> movie.getLikes() > 10).count();
-
-    System.out.println(count2);
+   var stream = Stream.generate(() -> Math.random());
+   stream.limit(3).forEach(n -> System.out.println(n));
   }
 }
