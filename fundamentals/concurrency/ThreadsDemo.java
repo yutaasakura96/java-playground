38950dff9829ca947e7e6d480bd7bd75fffd6e91
yuta asakura <yuta.asakura.se@gmail.com>
2025-04-09 11:08:41 +0900

    Refactor ThreadsDemo to streamline thread creation and improve readability

--- fundamentals/concurrency/ThreadsDemo.java
+++ fundamentals/concurrency/ThreadsDemo.java
@@ -2,11 +2,15 @@ package fundamentals.concurrency;
 
 public class ThreadsDemo {
   public static void show() {
-    System.out.println(Thread.currentThread().getName());
+    Thread thread = new Thread(new DownloadFileTask());
+    thread.start();
 
-    for (var i = 0; i < 10; i++) {
-      Thread thread = new Thread(new DownloadFileTask());
-      thread.start();
+    try {
+      thread.join();
+    } catch (InterruptedException e) {
+      e.printStackTrace();
     }
+
+    System.out.println("File is ready to be scanned");
   }
 }
