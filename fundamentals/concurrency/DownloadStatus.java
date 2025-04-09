c3245e84e1f6ecabd190cb5d922cc80635366a52
yuta asakura <yuta.asakura.se@gmail.com>
2025-04-09 13:53:23 +0900

    Refactor DownloadStatus to replace ReentrantLock with synchronized blocks for totalBytes and totalFiles increment

--- fundamentals/concurrency/DownloadStatus.java
+++ fundamentals/concurrency/DownloadStatus.java
@@ -1,23 +1,29 @@
 package fundamentals.concurrency;
 
-import java.util.concurrent.locks.Lock;
-import java.util.concurrent.locks.ReentrantLock;
-
 public class DownloadStatus {
   private int totalBytes;
-  private Lock lock = new ReentrantLock();
-
+  private int totalFiles;
+  private Object totalBytesLock = new Object();
+  private Object totalFilesLock = new Object();
 
   public int getTotalBytes() {
     return totalBytes;
   }
 
   public void incrementTotalbytes() {
-    lock.lock();
-    try {
+    synchronized (totalBytesLock) {
       totalBytes++;
-    } finally {
-      lock.unlock();
     }
   }
+
+  public void incrementTotalFiles() {
+    synchronized (totalFilesLock) {
+      totalFiles++;
+    }
+
+  }
+
+    public int getTotalFiles() {
+        return totalFiles;
+    }
 }
