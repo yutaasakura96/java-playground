3d63087b29244a8791be563e8df8a262f1cd45b8
yuta asakura <yuta.asakura.se@gmail.com>
2025-04-09 13:35:09 +0900

    Refactor DownloadStatus to improve thread safety in totalBytes increment

--- fundamentals/concurrency/DownloadStatus.java
+++ fundamentals/concurrency/DownloadStatus.java
@@ -1,13 +1,23 @@
 package fundamentals.concurrency;
 
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
 public class DownloadStatus {
   private int totalBytes;
+  private Lock lock = new ReentrantLock();
+
 
   public int getTotalBytes() {
     return totalBytes;
   }
 
   public void incrementTotalbytes() {
-    totalBytes++;
+    lock.lock();
+    try {
+      totalBytes++;
+    } finally {
+      lock.unlock();
+    }
   }
 }
