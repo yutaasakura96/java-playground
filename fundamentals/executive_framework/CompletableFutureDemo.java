8877f32c1dd0ed2ad0c024fe95ad9763a34aab06
yuta asakura <yuta.asakura.se@gmail.com>
2025-04-11 15:59:12 +0900

    Refactor CompletableFutureDemo to simplify asynchronous task handling and remove unused price calculation logic

--- fundamentals/executive_framework/CompletableFutureDemo.java
+++ fundamentals/executive_framework/CompletableFutureDemo.java
@@ -1,17 +1,25 @@
 package fundamentals.executive_framework;
 
 import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
 
 public class CompletableFutureDemo {
   public static void show() {
-    var first = CompletableFuture.supplyAsync(() -> "20USD")
-    .thenApply(str -> {
-      var price = str.replace("USD", "");
-      return Integer.parseInt(price);
-    });
-    var second = CompletableFuture.supplyAsync(() -> 0.9);
+    var first = CompletableFuture.supplyAsync(() -> 1);
+    var second = CompletableFuture.supplyAsync(() -> 2);
+    var third = CompletableFuture.supplyAsync(() -> 3);
 
-    first.thenCombine(second, (price, exchangeRate) -> price * exchangeRate)
-      .thenAccept(result -> System.out.println(result));
+    var all = CompletableFuture.allOf(first, second, third);
+    try {
+      var firstResult = first.get();
+      System.out.println(firstResult);
+    } catch (InterruptedException e) {
+      e.printStackTrace();
+    } catch (ExecutionException e) {
+      e.printStackTrace();
+    }
+
+    all.thenRun(() -> System.out.println("All tasks completed succesfully"));
+
   }
 }
