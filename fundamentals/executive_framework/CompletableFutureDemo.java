154b9cfc1073f537e3f9e4f5819e0acfc2db8de3
yuta asakura <yuta.asakura.se@gmail.com>
2025-04-11 15:06:30 +0900

    Refactor CompletableFutureDemo to simplify weather retrieval and convert temperature to Fahrenheit

--- fundamentals/executive_framework/CompletableFutureDemo.java
+++ fundamentals/executive_framework/CompletableFutureDemo.java
@@ -1,21 +1,14 @@
 package fundamentals.executive_framework;
 
 import java.util.concurrent.CompletableFuture;
-import java.util.concurrent.ExecutionException;
 
 public class CompletableFutureDemo {
+  public static int toFarenheit(int celsius) {
+    return (int) ((celsius * 1.8) + 32);
+  }
+
   public static void show() {
-    var future = CompletableFuture.supplyAsync(() -> {
-      System.out.println("Getting the current weather");
-      throw new IllegalStateException();
-    });
-    try {
-      var temperature = future.exceptionally(ex -> 1).get();
-      System.out.println(temperature);
-    } catch (InterruptedException e) {
-      e.printStackTrace();
-    } catch (ExecutionException e) {
-      e.printStackTrace();
-    }
+    var future = CompletableFuture.supplyAsync(() -> 20);
+    future.thenApply(CompletableFutureDemo::toFarenheit).thenAccept(f -> System.out.println(f));
   }
 }
